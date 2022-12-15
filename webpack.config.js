97f96a8d180ba57cd105f0d45a1a549550f8c6ed
Bruno Paraguassu Sousa <paraguassu.sousa@gmail.com>
2022-12-14 23:49:17 -0400

    Mini-projeto Webpack e Sass.

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: '[name].[contenthash].html',
+            template: './public/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new webpack.SourceMapDevToolPlugin({
+
+        }),
+        new HtmlMinimizerPlugin({
+
+        }),
+        new CssMinimizerPlugin({
+
+        }),
+
+
+    ],
+    optimization: {
+        minimize: true,
+    },
+    devtool: false,
+    module: {
+        rules: [
+
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    }
+}
